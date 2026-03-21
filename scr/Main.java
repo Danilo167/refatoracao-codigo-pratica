fae747227bee776b5958f410d5a01b811b7f78b2
Schei/εγώ <dscr9669@gmail.com>
2026-03-21 08:26:54 -0300

    Update Main.java

--- scr/Main.java
+++ scr/Main.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -6,3 +10,12 @@ public class Main {
         LocalDate dataRelatorio = LocalDate.of(2025, 9, 18);
 
         List<String> atividadesCarla = new ArrayList<>();
+        atividadesCarla.add("Reuniões com equipe");
+        atividadesCarla.add("Entregas do sprint");
+        atividadesCarla.add("Pendências");
+
+        String relatorio = ReportBuilder.montarRelatorio(carla, dataRelatorio, atividadesCarla);
+
+        System.out.println(relatorio);
+    }
+}
